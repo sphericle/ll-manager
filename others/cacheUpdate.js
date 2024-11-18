05ccbf18f2d90c2f52535454f89fca2cb4c6596d
sphericle <jackb2033@gmail.com>
2024-11-18 17:32:52 -0500

    its all over

--- others/cacheUpdate.js
+++ others/cacheUpdate.js
@@ -1,6 +1,7 @@
 const { githubBranch, githubDataPath, githubOwner, githubRepo } = require('../config.json');
 const logger = require('log4js').getLogger();
 const { cloneOrPullRepo, parseLevels, parseUsers } = require('./gitUtils.js');
+const { createUser } = require('../commands/records/records.js');
 module.exports = {
 	async updateCachedLevels() {
 		const { cache } = require('../index.js');
@@ -12,12 +13,20 @@ module.exports = {
 		const users = await parseUsers();
 		
 		if (users.length > 0) {
+			logger.info('Scheduled - ' + 'Parsing users...');
 			await cache.users.destroy({ where: {}});
 			try {
 				await cache.users.bulkCreate(users);
 				logger.info('Scheduled - ' + `Successfully updated ${users.length} cached users.`);
 			} catch (error) {
 				logger.error('Scheduled - ' + `Couldn't update cached users, something went wrong with sequelize: ${error}`);
+			}
+
+			try {
+				// await createUser('_', users);
+				logger.info('Scheduled - ' + `Successfully added ${users.length} users.`);
+			} catch (error) {
+				logger.error('Scheduled - ' + `Couldn't add users, something went wrong with sequelize: ${error}`);
 			}
 		}
 		
