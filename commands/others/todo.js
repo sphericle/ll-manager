dc1f0a6d2223e8fbe52a55c764d002ed9b38beba
sphericle <86196223+sphericle@users.noreply.github.com>
2024-12-08 15:17:16 -0500

    Update todo.js

--- commands/others/todo.js
+++ commands/others/todo.js
@@ -7,6 +7,6 @@ module.exports = {
 		.setDescription('Todo list for the bot')
 		.setDefaultMemberPermissions(0),
 	async execute(interaction) {
-		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level'});
+		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level\n- Command to say yes / no to a level in voting\n- Accept/reject a level'});
 	},
 };
