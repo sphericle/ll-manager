85d317bd922fcd9ee85d09be0df1ae52c33528b2
sphericle <86196223+sphericle@users.noreply.github.com>
2024-12-08 23:20:54 -0500

    Update todo.js

--- commands/others/todo.js
+++ commands/others/todo.js
@@ -7,18 +7,6 @@ module.exports = {
 		.setDescription('Todo list for the bot')
 		.setDefaultMemberPermissions(0),
 	async execute(interaction) {
-		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level\n- Command to say yes / no to a level in voting\n- Accept/reject a level'});
+		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level'});
 	},
 };
-
-
-/* 
-
-const text = "Red Green Yellow 2-0";
-const match = text.match(/(\d+)-\d+$/);
-const match2 = text.match(/\d+-(\d+)$/)
-if (match) {
-				console.log(match);
-    console.log(match[1]); // 2
-}
-*/
