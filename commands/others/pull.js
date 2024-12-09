454eb3782e45b7023f39f24bcb81092ed0d6f97a
sphericle <jackb2033@gmail.com>
2024-12-08 21:40:50 -0500

    git pull command

---
+++ commands/others/pull.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	enabled: true,
+	data: new SlashCommandBuilder()
+		.setName('pullchanges')
+		.setDefaultMemberPermissions(0)
+		.setDescription('Pull the latest changes from the repository'),
+	async execute(interaction) {
+		await interaction.reply('Pulling the latest changes from the repository...');
+
+		try {
+			const localRepoPath = path.resolve(`../../data/repo/`);
+			const { git } = require('../index.js');
+
+			await git.cwd(localRepoPath).pull();
+			return await interaction.editReply('Successfully pulled the latest changes from the repository');
+		} catch (error) {
+			return await interaction.editReply(':x: Something went wrong while fetching data from github, please try again later');
+		}
+	}
+}
