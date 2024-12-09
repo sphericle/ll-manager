0955ca6c180c2b6761d27293d2eb2f09aebba65e
sphericle <jackb2033@gmail.com>
2024-12-09 18:10:26 -0500

    test commit

--- commands/others/todo.js
+++ commands/others/todo.js
@@ -7,6 +7,6 @@ module.exports = {
 		.setDescription('Todo list for the bot')
 		.setDefaultMemberPermissions(0),
 	async execute(interaction) {
-		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level'});
+		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level\n- Deploy LOLOLLLOLOOL'});
 	},
 };
