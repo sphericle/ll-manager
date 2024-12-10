45ec6951f1d6c2e5a4ec2f5866facc81ce13a779
sphericle <86196223+sphericle@users.noreply.github.com>
2024-12-09 22:27:13 -0500

    Update ping.js

--- commands/others/ping.js
+++ commands/others/ping.js
@@ -4,7 +4,6 @@ module.exports = {
 	enabled: true,
 	data: new SlashCommandBuilder()
 		.setName('botping')
-		.setDefaultMemberPermissions(0)
 		.setDescription('Bot ping measurements'),
 	async execute(interaction) {
 		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true, ephemeral: true });
