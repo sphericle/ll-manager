282f64111103ff4ae346da4b88536a5a33e7059e
sphericle <86196223+sphericle@users.noreply.github.com>
2024-12-09 22:25:58 -0500

    Update todo.js

--- commands/others/todo.js
+++ commands/others/todo.js
@@ -4,8 +4,7 @@ module.exports = {
 	enabled: true,
 	data: new SlashCommandBuilder()
 		.setName('todo')
-		.setDescription('Todo list for the bot')
-		.setDefaultMemberPermissions(0),
+		.setDescription('Todo list for the bot'),
 	async execute(interaction) {
 		await interaction.reply({content: 'Todo: \n- Add command to copy a record to a different level\n- Deploy LOLOLLLOLOOL'});
 	},
