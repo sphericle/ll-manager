212aa95f24075bd84c31b358ead904545bd69590
sphericle <86196223+sphericle@users.noreply.github.com>
2024-12-11 22:23:07 -0500

    Update list.js

--- commands/list/list.js
+++ commands/list/list.js
@@ -396,7 +396,7 @@ module.exports = {
 					newCommit = await octokit.git.createCommit({
 						owner: githubOwner,
 						repo: githubRepo,
-						message: `Updated info for ${levelToEdit.name} (${interaction.user.name})`,
+						message: `Updated info for ${levelToEdit.name} (${interaction.user.tag})`,
 						tree: newTree.data.sha,
 						parents: [commitSha],
 					});
