399afcd931295fe8c93585220ed8241f96606967
sphericle <86196223+sphericle@users.noreply.github.com>
2024-12-19 19:46:10 -0500

    Dude

--- scheduled/cacheUpdate.js
+++ scheduled/cacheUpdate.js
@@ -10,25 +10,6 @@ module.exports = {
 	async execute() {
 		const { cache } = require('../index.js');
 		cache.updateLevels();
-
-
-		const users = await parseUsers();
-		if (users.length > 0) {
-			logger.info('Scheduled - Parsing users...');
-			await cache.users.destroy({ where: {} });
-			try {
-				await cache.users.bulkCreate(users);
-				logger.info(`Scheduled - Successfully updated ${users.length} cached users.`);
-			} catch (error) {
-				logger.error(`Scheduled - Couldn't update cached users, something went wrong with sequelize: ${error}`);
-			}
-
-			try {
-				await createUser('_', users);
-				logger.info(`Scheduled - Added ${users.length} users.`);
-			} catch (error) {
-				logger.error(`Scheduled - Couldn't add users, something went wrong with sequelize: ${error}`);
-			}
-		}
+		cache.updateUsers();
 	},
 };
