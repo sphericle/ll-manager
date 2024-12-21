5d211a50ed44cc966c4176efac506784e066e87e
GitHub Action <action@github.com>
2024-12-21 01:23:25 +0000

    CI: Automatic Format

--- bot.js
+++ bot.js
@@ -1,46 +1,55 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, staffGuildId, enableSeparateStaffServer } = require('./config.json');
-const log4js = require('log4js');
-const fs = require('node:fs');
-const path = require('node:path');
-const dotenv = require('dotenv');
+const { REST, Routes } = require("discord.js");
+const {
+  clientId,
+  guildId,
+  staffGuildId,
+  enableSeparateStaffServer,
+} = require("./config.json");
+const log4js = require("log4js");
+const fs = require("node:fs");
+const path = require("node:path");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 // Logger
-log4js.configure('./log4js.json');
+log4js.configure("./log4js.json");
 const logger = log4js.getLogger();
-const errorLogger = log4js.getLogger('error');
+const errorLogger = log4js.getLogger("error");
 
 // Error logging
-process.on('uncaughtException', (err) => {
-	errorLogger.error('Uncaught Exception:', err);
+process.on("uncaughtException", (err) => {
+  errorLogger.error("Uncaught Exception:", err);
 });
-  
-process.on('unhandledRejection', (reason, promise) => {
-	errorLogger.error('Unhandled Rejection at:', promise, 'reason:', reason);
+
+process.on("unhandledRejection", (reason, promise) => {
+  errorLogger.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
 const commands = [];
 // Grab all the command files from the commands directory
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command && 'enabled' in command) {
-			if (command.enabled) commands.push(command.data.toJSON());
-			else logger.info(`Ignoring disabled command ${filePath}`);
-		} else {
-			logger.info(`[WARNING] The command at ${filePath} is missing a required "data", "execute" or "enabled" property.`);
-		}
-	}
+  // Grab all the command files from the commands directory
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command && "enabled" in command) {
+      if (command.enabled) commands.push(command.data.toJSON());
+      else logger.info(`Ignoring disabled command ${filePath}`);
+    } else {
+      logger.info(
+        `[WARNING] The command at ${filePath} is missing a required "data", "execute" or "enabled" property.`
+      );
+    }
+  }
 }
 
 // Construct and prepare an instance of the REST module
@@ -48,23 +57,26 @@ const rest = new REST().setToken(process.env.TOKEN);
 
 // and deploy commands
 (async () => {
-	try {
-		logger.info(`Started refreshing ${commands.length} application (/) commands.`);
+  try {
+    logger.info(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-		if (enableSeparateStaffServer) {
-			await rest.put(
-				Routes.applicationGuildCommands(clientId, staffGuildId),
-				{ body: commands },
-			);
-		}
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+    if (enableSeparateStaffServer) {
+      await rest.put(Routes.applicationGuildCommands(clientId, staffGuildId), {
+        body: commands,
+      });
+    }
 
-		logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		logger.error(error);
-	}
+    logger.info(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    logger.error(error);
+  }
 })();
